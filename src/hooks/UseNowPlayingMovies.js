8443537888af06134b338ec191251d6a19826379
Deva9754 <deva.sahu2001@gmail.com>
2024-03-05 11:24:37 +0530

    "fix:build warning fixed"

--- src/hooks/UseNowPlayingMovies.js
+++ src/hooks/UseNowPlayingMovies.js
@@ -1,13 +1,13 @@
 import { useDispatch } from "react-redux";
 import { API_OPTIONS } from "../utils/Constants";
 import { addnowplayingmovies } from "../utils/Movieslice";
-import { useEffect } from "react";
+import { useEffect,useCallback } from "react";
 
 const UseNowPlayingMovie = () => {
   const dispatch = useDispatch();
   
 
-  const getNowPlaying = async () => {
+  const getNowPlaying =useCallback(async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
       API_OPTIONS
@@ -15,10 +15,10 @@ const UseNowPlayingMovie = () => {
     const json = await data.json();
      console.log(json?.results);
     dispatch(addnowplayingmovies(json.results));
-  };
+  },[dispatch]);
   useEffect(() => {
     getNowPlaying();
-  }, []);
+  }, [getNowPlaying]);
 };
 
 export default UseNowPlayingMovie;
