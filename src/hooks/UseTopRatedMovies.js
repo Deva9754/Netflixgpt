536768f725755de0c594316dfe7087eb6c8f3109
Deva9754 <deva.sahu2001@gmail.com>
2024-03-05 11:38:59 +0530

    "build warning eslint"

--- src/hooks/UseTopRatedMovies.js
+++ src/hooks/UseTopRatedMovies.js
@@ -1,12 +1,12 @@
 import { useDispatch } from "react-redux";
 import { API_OPTIONS } from "../utils/Constants";
 import { addTopRatedmovies } from "../utils/Movieslice";
-import { useEffect } from "react";
+import { useEffect,useCallback} from "react";
 
 const UseTopRatedMovies = () => {
   const dispatch = useDispatch();
 
-  const getTopRated = async () => {
+  const getTopRated =useCallback( async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/top_rated?page=1",
       API_OPTIONS
@@ -14,10 +14,10 @@ const UseTopRatedMovies = () => {
     const json = await data.json();
     // console.log(json?.results);
     dispatch(addTopRatedmovies(json.results));
-  };
+  },[dispatch]);
   useEffect(() => {
     getTopRated();
-  }, []);
+  }, [getTopRated]);
 };
 
 export default UseTopRatedMovies;
--- src/hooks/UseUpcomingMovies.js
+++ src/hooks/UseUpcomingMovies.js
@@ -1,12 +1,12 @@
 import { useDispatch } from "react-redux";
 import { API_OPTIONS } from "../utils/Constants";
 import { addUpcomingmovies } from "../utils/Movieslice";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 const UseUpcomingMovies = () => {
   const dispatch = useDispatch();
 
-  const getUpcoming = async () => {
+  const getUpcoming =useCallback( async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/upcoming?page=1",
       API_OPTIONS
@@ -14,10 +14,10 @@ const UseUpcomingMovies = () => {
     const json = await data.json();
     // console.log(json?.results);
     dispatch(addUpcomingmovies(json.results));
-  };
+  },[dispatch]);
   useEffect(() => {
     getUpcoming();
-  }, []);
+  }, [getUpcoming]);
 };
 
 export default UseUpcomingMovies;
