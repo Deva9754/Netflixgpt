86155a434e7d564f16b4a8bdd431233a223628fa
Deva9754 <deva.sahu2001@gmail.com>
2024-03-17 23:42:14 +0530

    "fix: api point open"

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,12 +1,13 @@
-export const URL_AVATAR ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&usqp=CAU";
+export const URL_AVATAR =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&usqp=CAU";
 
-
-export  const API_OPTIONS ={
-  method: 'GET',
+export const API_OPTIONS = {
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmZmYmU0OGI0Mzc2OTIyNzE2MzBjYjNmZDlhMzk4YyIsInN1YiI6IjY1ODAyOWEyODc1ZDFhMDdhYWFlODY4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f8VpOMolMTRP00dD9Wx6mIXRDjYGqd7q9BD3UoQmilM'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmZmYmU0OGI0Mzc2OTIyNzE2MzBjYjNmZDlhMzk4YyIsInN1YiI6IjY1ODAyOWEyODc1ZDFhMDdhYWFlODY4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f8VpOMolMTRP00dD9Wx6mIXRDjYGqd7q9BD3UoQmilM",
+  },
 };
 
 export const IMG_CDN = "https://image.tmdb.org/t/p/w200";
@@ -19,5 +20,5 @@ export const SUPPORTED_LANG = [
 export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-
-  export const OPEN_AI_KEY ="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";
+export const OPEN_AI_KEY =
+  "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";
