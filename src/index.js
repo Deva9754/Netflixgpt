fb1647b0d77a26ae035f65e27db47bc35d6446c9
Deva9754 <deva.sahu2001@gmail.com>
2024-05-23 23:43:35 +0530

    comment remove

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import appStore from './utils/Appstore';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import appStore from "./utils/Appstore";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <Provider store ={appStore}>
+  <Provider store={appStore}>
     <App />
-    </Provider>
-  // </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
