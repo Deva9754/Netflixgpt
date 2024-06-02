e3fa9fc4ebeafe52bb7a67cd4fb5fda7e9c077f4
Deva9754 <deva.sahu2001@gmail.com>
2024-06-02 15:41:16 +0530

    padding fix

--- src/shimmer/maincontainershimmer.js
+++ src/shimmer/maincontainershimmer.js
@@ -4,9 +4,13 @@ const MainContainerShimmer = () => {
   return (
     <div className="pt-[10%] bg-black opacity-80">
       <div className="py-[20%]">
-        <p className="text-4xl text-white text-center hover:animate-spin ">
+        {/* <p className="text-4xl text-white text-center hover:animate-spin ">
           Please wait Loading....🔃
-        </p>
+        </p> */}
+        <img
+          src="https://i.pinimg.com/originals/34/12/0d/34120d6eac6ceb48cabf121f84a9e3e1.gif"
+          alt="page-loader"
+        />
       </div>
     </div>
   );
