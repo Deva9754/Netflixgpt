bf18da86efd153f51b059aebc53f1f0777388cfe
Deva9754 <deva.sahu2001@gmail.com>
2024-06-02 15:11:51 +0530

    background img changed

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,14 +2,14 @@ import { signOut } from "firebase/auth";
 import { auth } from "../utils/Firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {  useEffect } from "react";
+import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/Userslice";
 import { onAuthStateChanged } from "firebase/auth";
 import { addGptMovieResult, toggleGptSearchView } from "../utils/GptSlice";
 import { changeLang } from "../utils/ConfigSlice";
 import { SUPPORTED_LANG } from "../utils/Constants";
 
- const Header = () => {
+const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
@@ -35,7 +35,6 @@ import { SUPPORTED_LANG } from "../utils/Constants";
     dispatch(addGptMovieResult({ movieResults: null, movieNames: null }));
   };
 
-
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -49,25 +48,25 @@ import { SUPPORTED_LANG } from "../utils/Constants";
         navigate("/");
         // ...
       }
-    })
-    return ()=>unsubscribe();
+    });
+    return () => unsubscribe();
     // eslint-disable-next-line
   }, []);
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
       <img
-      className="mx-auto md:mx-0 w-44 "
+        className="mx-auto md:mx-0  object-fill opacity-80 rounded-xl cursor-pointer "
         height={60}
-        width={146}
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        width={110}
+        src="https://s3.amazonaws.com/logos-brandpa-com/uploads/watchita-logo-thumbnail.png"
         alt="Netflix logo"
-      ></img>{
-        user && (
-          <div className=" flex pl-24 justify-items-end">
-            {showGptSearch && (
-                           <select
-              className="p-2 m-2 bg-purple-300 text-white rounded-xl font-semibold"
+      ></img>
+      {user && (
+        <div className=" flex pl-24 justify-items-end">
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-[#0f79af] text-white rounded-xl font-bold"
               onChange={handleLangChange}
             >
               {SUPPORTED_LANG.map((lang) => (
@@ -76,16 +75,19 @@ import { SUPPORTED_LANG } from "../utils/Constants";
                 </option>
               ))}
             </select>
-            )}
-            <button className=" py-2 px-4 mx-4 my-2 bg-purple-700 rounded-lg" onClick={handleGptSearchClick}>
-               {showGptSearch ? "Home" : "GPT Search"}
-               </button>
-        </div>)
-      }
+          )}
+          <button
+            className=" py-2 px-4 mx-4 my-2 bg-[#0f79af] rounded-lg text-white"
+            onClick={handleGptSearchClick}
+          >
+            {showGptSearch ? "Home" : "GPT Search"}
+          </button>
+        </div>
+      )}
       {user ? (
         <div className="flex gap-2 items-center">
           <img
-           className="hidden md:inline-block w-12 h-12 rounded-full "
+            className="hidden md:inline-block w-12 h-12 rounded-full "
             alt="loading"
             src="https://cdn1.vectorstock.com/i/1000x1000/31/95/user-sign-icon-person-symbol-human-avatar-vector-12693195.jpg"
           />
@@ -98,4 +100,4 @@ import { SUPPORTED_LANG } from "../utils/Constants";
   );
 };
 
- export default Header;
+export default Header;
