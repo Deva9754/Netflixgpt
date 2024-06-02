ed2d4cd2fe6735e1ad097aa62d1e116fad5d3753
Deva9754 <deva.sahu2001@gmail.com>
2024-06-02 15:40:57 +0530

    margin fixed

--- src/component/MovieList.js
+++ src/component/MovieList.js
@@ -7,16 +7,15 @@ const MovieList = ({ title, movies }) => {
     <div className=" px-6">
       <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
 
-
-      <div className=" flex overflow-x-scroll ">
+      <div className=" flex overflow-x-scroll mt-12 ">
         <div className="flex">
           {movies ? (
-          movies?.map((movie) => (
-            <MovieCard key={movie.id} posterpath={movie.poster_path} />
-          ))
-        ):(
-            <ShimmerMovies/>
-        )}
+            movies?.map((movie) => (
+              <MovieCard key={movie.id} posterpath={movie.poster_path} />
+            ))
+          ) : (
+            <ShimmerMovies />
+          )}
         </div>
       </div>
     </div>
