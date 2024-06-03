d21e59cb958af4142f555f703366dd862004b46f
Deva9754 <deva.sahu2001@gmail.com>
2024-06-03 21:20:53 +0530

    console fixed

--- src/hooks/UseNowPlayingMovies.js
+++ src/hooks/UseNowPlayingMovies.js
@@ -1,11 +1,10 @@
 import { useDispatch } from "react-redux";
 import { API_OPTIONS } from "../utils/Constants";
 import { addnowplayingmovies } from "../utils/Movieslice";
-import { useEffect,useCallback } from "react";
+import { useEffect, useCallback } from "react";
 
 const UseNowPlayingMovie = () => {
   const dispatch = useDispatch();
-  
 
   const getNowPlaying = useCallback(async () => {
     const data = await fetch(
@@ -13,9 +12,10 @@ const UseNowPlayingMovie = () => {
       API_OPTIONS
     );
     const json = await data.json();
-    //  console.log(json?.results);
+    //console.log(json?.results);
+    // console.log(JSON.stringify(json.results, null, 2));
     dispatch(addnowplayingmovies(json.results));
-  },[dispatch]);
+  }, [dispatch]);
   useEffect(() => {
     getNowPlaying();
   }, [getNowPlaying]);
